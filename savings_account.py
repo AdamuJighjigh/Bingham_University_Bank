1d3c2557777a668b58a0492dc899a40ea79838da
Adamu Jighjigh <0j144k@gmail.com>
2024-06-06 16:01:45 +0100

    savings account created

---
+++ savings_account.py
@@ -0,0 +1,21 @@
+from account import Account
+
+class SavingsAccount(Account):
+    def __init__(self):
+        Account.__init__(self)
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        rate = self.balance * (0.5/100)
+        self.balance = self.balance + rate
+        print("New balance is: ", self.balance)
+
+    def withdrawal(self, amount):
+        if amount <= 700000:
+            super().withdrawal(amount)
+        else:
+            print("Amount exceeds withdrawal limit", self.__interest_rate)
+
+savings = SavingsAccount()
+savings.withdrawal(1000)
+savings.deposit(0)
