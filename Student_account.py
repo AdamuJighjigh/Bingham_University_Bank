fb13a9e3a77d468d840dc711b282d7c3b68d17f4
Ibrahim Daniel <dado.dd21@gmail.com>
2024-06-06 16:42:17 +0100

    Added withdraw limit message

--- Student_account.py
+++ Student_account.py
@@ -3,12 +3,11 @@ from account import Account
 class StudentAccount(Account):
     def __init__(self):
         Account.__init__(self)
-
     def withdrawal(self, amount):
-        if amount <= 2000:
+        if amount <=2000:
             super().withdrawal(amount)
         else:
-            print("Amount exceeds withdrawal limit")
+            print("Withdraw limit exceeded")
 
     def deposit(self, amount):
         if amount <= 50000:
@@ -17,5 +16,5 @@ class StudentAccount(Account):
             print("amount exceeds deposit limit!")
 
 student = StudentAccount()
-student.withdrawal(1000)
-student.deposit(2000)
+student.withdrawal(1999)
+student.deposit(1000)
