1a22619d01a13ff84cc1a7cc7a1455863076e65a
Adamu Jighjigh <0j144k@gmail.com>
2024-06-06 16:00:00 +0100

    child account created

---
+++ children_account.py
@@ -0,0 +1,18 @@
+from account import Account
+
+class ChildrenAccount(Account):
+    def __init__(self):
+        Account.__init__(self)
+
+    def deposit(self, amount):
+        self.__interest = amount * (0.7/100)
+        self.balance = self.balance + amount
+        self.balance = self.balance + self.__interest
+        print("New balance is: ", self.balance)
+
+    def withdrawal(self):
+        print("This function is not available to this account")
+
+children = ChildrenAccount()
+children.deposit(7000)
+children.withdrawal()
