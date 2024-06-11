b7d36600e13a1b41eedea30673464d1539eed2d6
Ibrahim Daniel <dado.dd21@gmail.com>
2024-06-11 13:02:07 +0100

    updates

--- current_account.py
+++ current_account.py
@@ -1,9 +1,15 @@
 from account import Account
 
-class CurrentAccount(Account):
+class CurrentAccount (Account):
+
+
     def __init__(self):
         Account.__init__(self)
 
-current = CurrentAccount()
+current =  CurrentAccount()
+
+
 current.deposit(5000)
+
+
 current.withdrawal(1000)
